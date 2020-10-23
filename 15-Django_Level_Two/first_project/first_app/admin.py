7e276d4937bb3fdad871aa8cadfc82f7f0cdce8d
tcloud1105 <68422485+tcloud1105@users.noreply.github.com>
2020-10-23 09:49:58 +0200

    Add files via upload

---
+++ 15-Django_Level_Two/first_project/first_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Topic,Webpage,AccessRecord
+# Register your models here.
+admin.site.register(Topic)
+admin.site.register(Webpage)
+admin.site.register(AccessRecord)
+
+#create the superuser
+#python manage.py createsuperuser
